05181761a87f78e4fc93011d5769f91c62882be3
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-02-14 16:25:32 -0800

    Adding alphabetical sorting to the select dropdown menu

--- trizent/src/components/Filter.jsx
+++ trizent/src/components/Filter.jsx
@@ -64,14 +64,14 @@ function Filter({ newFilter, filter, handleFilterChange, handleFilterSubmit }) {
                 )
               })}
             </select>
-            <p className=' peer-focus/name:block hidden absolute -top-2 text-[0.7rem] bg-white ml-2 px-1 font-bold text-center md:text-lg md:-top-4'>Year</p>
+            {/* <p className=' peer-focus/name:block hidden absolute -top-2 text-[0.7rem] bg-white ml-2 px-1 font-bold text-center md:text-lg md:-top-4'>Year</p> */}
           </div>
 
           <div className='relative w-[17%] md:w-[49%]'>
              <select name="Make" value={filter.Make} onChange={handleFilterChange} type="text" autoComplete='off' placeholder="Year-" className='peer/name focus:placeholder:invisible border border-violet-950 h-7 text-xs text-xs w-full rounded placeholder:text-sm'>
               <option defaultValue="Make" >Make</option>
               {/* <option selected={false} >Make</option> */}
-              {data.make.map(item => {
+              {data.make.sort().map(item => {
                 return (
                   <option className='text-sm'>{item}</option>
                 )
@@ -84,7 +84,7 @@ function Filter({ newFilter, filter, handleFilterChange, handleFilterSubmit }) {
           { filter.Make ? 
              <select name="Model" value={filter.Model} onChange={handleFilterChange} type="text" autoComplete='off' placeholder="Year-" className='peer/name focus:placeholder:invisible border border-violet-950 h-7 text-xs w-full rounded placeholder:text-sm'>
              <option disabled  >Model</option>
-             {data.model.filter(item => Object.keys(item)[0] === filter.Make).map(item => {
+             {data.model.sort().filter(item => Object.keys(item)[0] === filter.Make).map(item => {
                return (
                  <option className='text-sm'>{item[filter.Make]}</option>
                )
@@ -92,7 +92,7 @@ function Filter({ newFilter, filter, handleFilterChange, handleFilterSubmit }) {
            </select> : 
             <select name="Model" value={filter.Model} onChange={handleFilterChange} type="text" autoComplete='off' placeholder="Year-" className='peer/name focus:placeholder:invisible border border-violet-950 h-7 text-xs w-full rounded placeholder:text-sm'>
             <option defaultValue="Model" >Model</option>
-            {copyOptions.Model.map(item => {
+            {copyOptions.Model.sort().map(item => {
               return (
                 <option className='text-sm'>{item}</option>
               )
@@ -107,7 +107,7 @@ function Filter({ newFilter, filter, handleFilterChange, handleFilterSubmit }) {
           { filter.Make ? 
              <select name="Category" value={filter.Category} onChange={handleFilterChange} type="text" autoComplete='off' placeholder="Year-" className='peer/name focus:placeholder:invisible border border-violet-950 h-7 text-xs w-full rounded placeholder:text-sm'>
              <option selected={false} >Category</option>
-             {data.category.filter(item => Object.keys(item)[0] === filter.Make).map(item => {
+             {data.category.sort().filter(item => Object.keys(item)[0] === filter.Make).map(item => {
                return (
                  <option className='text-sm'>{item[filter.Make]}</option>
                )
@@ -115,7 +115,7 @@ function Filter({ newFilter, filter, handleFilterChange, handleFilterSubmit }) {
            </select> : 
             <select name="Category" value={filter.Category} onChange={handleFilterChange} type="text" autoComplete='off' placeholder="Year-" className='peer/name focus:placeholder:invisible border border-violet-950 h-7 text-xs w-full rounded placeholder:text-sm'>
             <option selected={false} >Category</option>
-            {copyOptions.Category.map(item => {
+            {copyOptions.Category.sort().map(item => {
               return (
                 <option className='text-sm'>{item}</option>
               )
@@ -128,7 +128,7 @@ function Filter({ newFilter, filter, handleFilterChange, handleFilterSubmit }) {
           <div className='relative w-[17%] md:w-[49%]'>
           <select name="Color" value={filter.Color} onChange={handleFilterChange} type="text" autoComplete='off' placeholder="Year-" className='peer/name focus:placeholder:invisible border border-violet-950 h-7 text-xs w-full rounded placeholder:text-sm'>
               <option selected={false} >Color</option>
-              {data.color.map(item => {
+              {data.color.sort().map(item => {
                 return (
                   <option className='text-sm'>{item}</option>
                 )
